04aa895ae3f1b3165f521bb132e5146d05552c29
Jandi95 <dduk2684@naver.com>
2025-05-31 22:46:46 +0900

    [feature] main 페이지 마크업 및 슬라이드 라이브러리 적용

---
+++ src/page/main-page/main-page.js
@@ -0,0 +1,71 @@
+$('.main-cont-list.important, .main-cont-list.popular').slick({
+  infinite: false,
+  arrows: false,
+  slidesToShow: 8.3,
+  slidesToScroll: 2,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 5.3,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3.3,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+$('.main-cont-list.quick, .main-cont-list.live, .main-cont-list.event').slick({
+  infinite: false,
+  arrows: false,
+  slidesToShow: 5.3,
+  slidesToScroll: 2,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3.3,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2.3,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+$('.main-cont-list.only').slick({
+  infinite: false,
+  arrows: false,
+  slidesToShow: 6.2,
+  slidesToScroll: 2,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4.2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2.2,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
